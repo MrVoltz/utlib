5547a9707d5c45f4c3c24298717a43a0155929b0
MrVoltz <uzivatelmp@gmail.com>
2021-12-04 12:18:14 +0100

    Update

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const { resolveCaptcha: resolveCaptchaBrowser } = require("./lib/captcha-browser"),
-	{ generateLink } = require("./lib/link-generator");
+const Promise = require("bluebird"),
+	readline = require("readline");
 
-function extractFileSlug(url) {
-	var m = url.match(/\/file\/([^/]+)\//);
-	return m ? m[1] : null;
+const { resolveCaptcha: resolveCaptchaBrowser } = require("./lib/captcha-browser"),
+	{ generateLink } = require("./lib/link-generator"),
+	{ extractFileSlug } = require("./lib/url-extractor");
+
+function processUrl(url) {
+	return Promise.try(() => {
+		url = url.trim();
+		if(!url)
+			throw new Error("No url specified.");
+
+		return extractFileSlug(url);
+	}).then(fileSlug => {
+		if(!fileSlug)
+			throw new Error("Invalid url.");
+
+		return generateLink(fileSlug, resolveCaptchaBrowser);
+	}).then(link => {
+		console.log(`Link: ${link}`);
+	}).catch(err => {
+		console.error(`Error: ${err.message || err}`);
+	});
 }
+
+function interactive() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	});
+
+	function loop() {
+		return (new Promise(resolve => {
+			rl.question("URL: ", resolve);
+		})).then(processUrl).then(loop);
+	}
+
+	return loop();
+}
+
 
 function main() {
 	let url = process.argv[2];
-	if(!url) {
-		console.error("No url specified.");
-		return;
-	}
+	if(!url)
+		return interactive();
 
-	let fileSlug = extractFileSlug(url);
-	if(!fileSlug) {
-		console.error("Invalid url.");
-		return;
-	}
-
-	generateLink(fileSlug, resolveCaptchaBrowser).then(link => {
-		console.log("link: " + link);
-	}).catch(err => {
-		console.log("error", err);
-	});
+	return processUrl(url);
 }
 
 main();
